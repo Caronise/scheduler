fba07218f400a0f9b81acd40f67410e2fa1b64df
Caronise <lucas.betancourt@hotmail.com>
2019-11-26 17:08:47 -0500

    Created InterviewerList and InterviewerListItem components and tested they work properly... started creating the Appointment components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+
+export default function InterviewerList({ interviewers, value, onChange }) {
+  
+  const listOfInterviewers = interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === value}
+        setInterviewer={event => onChange(interviewer.id)}
+      />
+    );
+  });
+  
+  return (
+
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer:</h4>
+      <ul className="interviewers__list">
+        {listOfInterviewers}
+      </ul>
+    </section>
+
+  );
+};
