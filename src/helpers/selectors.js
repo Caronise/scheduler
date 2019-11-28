805a7c6c48a7ee7cf7c729fab024953ea1d4b95c
Caronise <lucas.betancourt@hotmail.com>
2019-11-28 13:56:56 -0500

    Created getInterview function, tested it and modified Application.js accordingly, created empty folders for hooks and testing hooks

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,20 +2,35 @@ export function getAppointmentsForDay(state, day) {
 
   const filteredAppointments = [];
   
-  const selectedDay = state.days.filter(d => d.name === day)
-  
-  // console.log("=========================================");
-  // console.log("state is : ", selectedDay[0]);
-  // console.log("=========================================");
-  
+  const selectedDay = state.days.filter(d => d.name === day);
+
   if (selectedDay[0] === undefined)
     return [];
   
   selectedDay[0].appointments.forEach(appointmentId => {
-    if(String(appointmentId) in state.appointments)
-      filteredAppointments.push(state.appointments[appointmentId])
-  })
+    if(String(appointmentId) in state.appointments) {
+      filteredAppointments.push(state.appointments[appointmentId]);
+    };
+  });
   return filteredAppointments;
   
-}
+};
 
+export function getInterview(state, interview) {
+  const interviewer = {};
+  
+  if (!interview){
+    return null;
+  }
+  
+  // student name and interviewer id are found in
+  const studentName = interview.student 
+  const interviewerId = interview.interviewer
+
+  // interviewer info is found in
+  const interviewerInfo = state.interviewers[interviewerId]
+
+  interviewer.student = studentName;
+  interviewer.interviewer = interviewerInfo;
+  return interviewer;
+};
