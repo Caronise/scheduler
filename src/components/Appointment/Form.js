a54f1c82d4d496d8c954b7cd70fedcc21d0c7a4e
Caronise <lucas.betancourt@hotmail.com>
2019-12-03 20:36:28 -0500

    Created tests for the Form component and made sure they passed, in the <input> name was changed to value and defaultvalue was removed to pass the tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,8 @@ export default function Form({ name, interviewers, interviewer, onSave, onCancel
 
   const [stateName, setStateName] = useState(name || "");
   const [stateInterviewer, setStateInterviewer] = useState(interviewer || null);
+  const [error, setError] = useState("");
+
 
   const reset = function() {
     setStateName("");
@@ -17,25 +19,36 @@ export default function Form({ name, interviewers, interviewer, onSave, onCancel
     onCancel();
   }
 
+  function validate() {
+    if (stateName === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    onSave(stateName, stateInterviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
-            onChange={(event) => setStateName(event.target.value)}
             className="appointment__create-input text--semi-bold"
-            name={stateName}
+            value={stateName}
             type="text"
             placeholder="Enter Student Name"
-            defaultValue={name}
+            onChange={(event) => setStateName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={interviewers} value={stateInterviewer} onChange={setStateInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={() => onSave(stateName, stateInterviewer)}>Save</Button>
+          <Button confirm onClick={() => validate(stateName, stateInterviewer)}>Save</Button>
         </section>
       </section>
     </main>
